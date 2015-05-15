e54cfd06d6ff1f4869b5be2ae7f972dddae64874
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-15 13:02:02 +0600

    version 02

--- app/src/main/java/kz/abcsoft/manual/DetailActivty.java
+++ app/src/main/java/kz/abcsoft/manual/DetailActivty.java
@@ -1,9 +1,18 @@
 package kz.abcsoft.manual;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class DetailActivty extends ActionBarActivity {
@@ -12,6 +21,38 @@ public class DetailActivty extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WebView webView = (WebView)findViewById(R.id.webView) ;
+
+        Intent intent = getIntent() ;
+
+        String resName = "n" + intent.getIntExtra("head", 0) ;
+        Log.i("name", resName) ;
+        Context context = getBaseContext() ;
+        String text = readRawTextFile(context, getResources().getIdentifier(resName, "raw",
+                "kz.abcsoft.manual")) ;
+        webView.loadDataWithBaseURL(null, text, "text/html", "utf-8", null);
+
+    }
+
+    public static String readRawTextFile(Context context, int resId)
+    {
+        InputStream inputStream = context.getResources().openRawResource(resId);
+
+        InputStreamReader inputReader = new InputStreamReader(inputStream);
+        BufferedReader buffReader = new BufferedReader(inputReader);
+        String line;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            while (( line = buffReader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return builder.toString();
     }
 
     @Override
